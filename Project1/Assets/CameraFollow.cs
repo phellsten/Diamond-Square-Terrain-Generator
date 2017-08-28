f2aa277a027f8d4c8374faf01e300c14515e94cc
Paul Hellsten <paulthegreen@live.com>
2017-08-28 14:15:47 +1000

    Restrict camera to terrain

--- Project1/Assets/CameraFollow.cs
+++ Project1/Assets/CameraFollow.cs
@@ -6,17 +6,14 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject target;
-    // Use this for initialization
+    // Initialisation of game variables
+    private float maxX = 1024f;
+    private float maxZ = 1024f;
+    private float minZ = 0;
+    private float minX = 0;
+    private int speed = 25;
 
-    void Start()
-    {
-
-    }
-
-    int speed = 25;
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 		// Rolling of camera.
 		if (Input.GetKey("e")) {
 			transform.RotateAroundLocal (-transform.forward, 0.025f);
@@ -25,7 +22,7 @@ public class CameraFollow : MonoBehaviour
 			transform.RotateAroundLocal (transform.forward, 0.025f);
 		}
 
-				// Mouse movement causes direction of camera to rotate.
+		// Mouse movement causes direction of camera to rotate.
 		if (Input.GetAxis ("Mouse X") != 0f) {
 			transform.Rotate (new Vector3 (0f, Input.GetAxis("Mouse X")));
 		}
@@ -36,17 +33,16 @@ public class CameraFollow : MonoBehaviour
 		// Shift to move faster.
 		if (Input.GetKey(KeyCode.LeftShift)) {
 			speed = 25 * 10;
-		} else {
+		}
+        else {
 			speed = 25;
 		}
 
 		// Move relative to the direction faced.
-		if (Input.GetKey("w"))
-		{
-			transform.position += this.transform.forward * speed * Time.deltaTime;
+		if (Input.GetKey("w")) {
+            transform.position += this.transform.forward * speed * Time.deltaTime;
 		}
-		if (Input.GetKey("s"))
-		{
+		if (Input.GetKey("s")) {
 			transform.position -= this.transform.forward * speed * Time.deltaTime;
 		}
 		if (Input.GetKey ("a")) {
@@ -55,5 +51,19 @@ public class CameraFollow : MonoBehaviour
 		if (Input.GetKey ("d")) {
 			transform.position += this.transform.right * speed * Time.deltaTime;
 		}
+
+        // Restricting camera movement to above the terrain
+        if(transform.position.x > maxX) {
+            transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < minX) {
+            transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z > maxZ) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, maxZ);
+        }
+        else if (transform.position.x < minZ) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, minZ);
+        }
     }
 }
